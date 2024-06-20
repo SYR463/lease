36880859717202f58611df1f72a09f5f159304ba
A1075 <yrshao@foxmail.com>
2024-06-20 19:02:07 +0800

    房间支付方式管理 + 房间租期管理

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/PaymentTypeController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/PaymentTypeController.java
@@ -3,8 +3,11 @@ package com.atguigu.lease.web.admin.controller.apartment;
 
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.PaymentType;
+import com.atguigu.lease.web.admin.service.PaymentTypeService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,21 +18,35 @@ import java.util.List;
 @RestController
 public class PaymentTypeController {
 
+    @Autowired
+    private PaymentTypeService paymentTypeService;
+
     @Operation(summary = "查询全部支付方式列表")
     @GetMapping("list")
     public Result<List<PaymentType>> listPaymentType() {
-        return Result.ok();
+
+//        // 增加过滤条件，过滤掉 逻辑删除标号为1的
+//        LambdaQueryWrapper<PaymentType> queryWrapper = new LambdaQueryWrapper<>();
+//        queryWrapper.eq(PaymentType::getIsDeleted, 0);
+
+        List<PaymentType> list = paymentTypeService.list();
+
+        return Result.ok(list);
     }
 
     @Operation(summary = "保存或更新支付方式")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdatePaymentType(@RequestBody PaymentType paymentType) {
+
+        paymentTypeService.saveOrUpdate(paymentType);
+
         return Result.ok();
     }
 
     @Operation(summary = "根据ID删除支付方式")
     @DeleteMapping("deleteById")
     public Result deletePaymentById(@RequestParam Long id) {
+        paymentTypeService.removeById(id);
         return Result.ok();
     }
 
